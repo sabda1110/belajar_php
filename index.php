21b48e713bc87995b908a7159ceb88dceb179d0e
sabda1110 <sabdasetiawan205@gmail.com>
2021-07-22 20:17:52 +0700

    Belajar Control Flow PHP

--- index.php
+++ index.php
@@ -1,47 +1,30 @@
-<?php
- //Penulisan HTML dalam PHP
- echo "<h1>Sabda Setiawan </h1>"; 
-  $nama = "SABDA";
-?>
-
 <!DOCTYPE html>
 <html lang="en">
-<!-- Penulisan PHP dalam HTML -->
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Pertemuan 5 PHP Control Flow</title>
+    <style>
+        .warna{
+            background-color:red;
+             }
+    </style>
 </head>
 <body>
-    <h1><?php echo $nama; ?></h1>
+    <table border="1" cellpadding="20" cellspacing="0">
+     <?php for($i=1;$i<6;$i++): ?>
+       <?php if($i%2==0):?>
+           <tr class="warna"> 
+        <?php else: ?>
+            <tr>
+        <?php endif; ?>         
+        <?php for($j=1;$j<=5;$j++) : ?>
+        <td><?= $i.",".$j; ?></td>
+         <?php endfor; ?>  
+      </tr>     
+      <?php endfor; ?>
+
+    </table>
 </body>
 </html>
-
-
-
-<?php
-//OPERATOR
-//Aritmatika (+ , - , / , * , %)
-$a=5;
-$b=6;
-echo $a+$b;
-
-//Pengabungan Cincatenation  (.)
-$nama="sabda";
-$belakang = "Setiawan"
-echo $nama." ".$belakang;
-
-//Assigment (= , += , -= , /= ,*= , %=, .=)
-$a=5;
-$a *= 2;
-
-//Perbandingan (>,<,>=,<=,==,!=)
-var_dump($a<$b);
-
-//Identitas dari sebua nilai
-var_dump($b === "6");
-
-//Logika
-var_dump($a>60 && $b<20);
-?>
