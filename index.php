1f00be8ef236c554ce904750adfc5549fa5974eb
sabda1110 <sabdasetiawan205@gmail.com>
2021-07-24 20:00:16 +0700

    Belajar pemanggilan array

--- index.php
+++ index.php
@@ -1,14 +1,6 @@
 <?php
-function tgl_lahir($nama="admin",$lahir){
-    echo "Selamat Datang $nama <br>";
-    echo "NAMA :".$nama."<br>";
-    echo "TGL  :".$lahir."<br>";
-    echo "Hari :".date("l",strtotime($lahir));
-}
-
+$arr1=[1,3,5,4,2,1,1];
 ?>
-
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -16,9 +8,34 @@ function tgl_lahir($nama="admin",$lahir){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Belajar User Defined Function</title>
+    <title>Belajar Array</title>
+    <style>
+
+        .contai{
+            display:flex;
+            width: 750px;
+            flex-wrap:wrap;
+        }
+        .kotak{
+            width: 100px;
+            height: 100px;
+            background-color:red;
+            margin:3px;
+            text-align:center;
+            line-height:100px;  
+        }
+    </style>
 </head>
 <body>
-    <?php tgl_lahir("Sabda","12 octo 2000");  ?>
+    <div class="contai">
+    <?php for($i=0;$i<count($arr1);$i++) : ?>
+        <div class="kotak"><?= $arr1[$i];  ?></div>
+    <?php endfor ?>
+
+    <?php foreach($arr1 as $a) : ?>
+     <div class="kotak"><?= $a ?></div>
+     <?php endforeach ?>   
+    </div>
 </body>
 </html>
+
